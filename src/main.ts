49d82236a77c498f9605a1e12a8659758c08dd99
daorozc <daorozc@bancolombia.com.co>
2023-09-06 11:12:22 -0500

    Add Swagger documentation generation using "@nestjs/swagger" package to provide API documentation for the users API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Users example')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const DEFAULT_PORT = 3000;
   const port = process.env.PORT || DEFAULT_PORT;
   await app.listen(port);
 }
+
 bootstrap();
